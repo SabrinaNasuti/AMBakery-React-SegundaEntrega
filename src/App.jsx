0e7bc43a19d7b59ded018165985b3ea59ea3b390
Sabrina Nasuti <99421741+SabrinaNasuti@users.noreply.github.com>
2024-02-26 21:40:27 -0300

    Error 404

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
           <Route path="/section/:section" element={<ItemListContainer />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1>Error 404 :o </h1>} />
         </Routes>
       </BrowserRouter>
     </>
